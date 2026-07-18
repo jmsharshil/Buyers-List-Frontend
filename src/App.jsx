c51f31361d4755b4d28c016ddc23d0de4b420a0c
Aryan072003 <aryansojitra55@gmail.com>
2026-07-18 16:55:41 +0530

    Fixed Router Import Path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import router from "./routes/router";
+import router from "./routes/Router";
 
 import store from "./store/store";
 
